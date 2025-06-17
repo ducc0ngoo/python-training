33a97b012a396fe7c43be592d01c3c361e3defde
ducc0ngoo <ngocduc20102006@gmail.com>
2025-06-17 07:52:39 +0700

    init commit

---
+++ main.py
@@ -0,0 +1,18 @@
+"""
+int: 1 2 100 0 -100
+float: 2.5, -.5, .5
+str: "", ''
+bool: True/False
+"""
+
+# comment
+print(100)
+print("Nguyen Ngoc Duc")
+print(2, 2)
+"""
+print(1, 2, 3, 4, sep=" * ")
+
+print(True, False, end='?\n')
+"""
+print("💚💚")
+# print('john')
